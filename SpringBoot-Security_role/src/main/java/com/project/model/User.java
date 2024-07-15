72f6ffba304f7da9b5af041d080fd809782773ba
chhomdavit1992 <xiaoming00009@gmail.com>
2024-07-15 18:45:51 +0700

    code 7/15/2024

--- SpringBoot-Security_role/src/main/java/com/project/model/User.java
+++ SpringBoot-Security_role/src/main/java/com/project/model/User.java
@@ -4,93 +4,92 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users") 
 public class User {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
-	private String name;
+    private String name;
 
-	private String mobileNo;
+    private String mobileNo;
 
-	private String email;
+    private String email;
 
-	private String password;
+    private String password;
 
-	private String role;
+    private String role;
 
-	public User() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+    public User() {
+        super();
+    }
 
-	public User(int id, String name, String mobileNo, String email, String password, String role) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.mobileNo = mobileNo;
-		this.email = email;
-		this.password = password;
-		this.role = role;
-	}
-	
+    public User(int id, String name, String mobileNo, String email, String password, String role) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.mobileNo = mobileNo;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getMobileNo() {
-		return mobileNo;
-	}
+    public String getMobileNo() {
+        return mobileNo;
+    }
 
-	public void setMobileNo(String mobileNo) {
-		this.mobileNo = mobileNo;
-	}
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public String getRole() {
-		return role;
-	}
+    public String getRole() {
+        return role;
+    }
 
-	public void setRole(String role) {
-		this.role = role;
-	}
+    public void setRole(String role) {
+        this.role = role;
+    }
 
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", mobileNo=" + mobileNo + ", email=" + email + ", password="
-				+ password + ", role=" + role + "]";
-	}
-	
-	
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + ", mobileNo=" + mobileNo + ", email=" + email + ", password="
+                + password + ", role=" + role + "]";
+    }
 }
+
